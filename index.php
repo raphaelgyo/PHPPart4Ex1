6b3b813d916755d0f839412cac7f141ff6e2144f
raphaelgyo <raphael.granizo@gmail.com>
2019-12-04 18:48:56 +0100

    1st commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>PHP Partie 4 Exercice 1 : Les fonctions</title>
+  </head>
+  <body>
+    <?php
+    function carre($num){
+      return $num * $num;
+    }
+    echo carre(4); // Affiche '16'
+    ?>
+  </body>
+</html>
